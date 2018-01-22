1b8d2a1d702a41c7b562db7fa395f4815b54be7d
Evan Stern <evanmicahstern@gmail.com>
2018-01-21 21:42:13 -0500

    Add Socket.IO

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import cors from 'cors';
+// import { Server } from 'http';
+// import SocketIO from 'socket.io';
 import { port } from './config';
 import logger from './services/logger';
+
+const app = express();
+
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// io.listen(server);
+
+// const server = new Server(app);
+// const io = SocketIO.listen(server);
 
 logger.stream = {
   write: (message) => {
@@ -8,12 +20,28 @@ logger.stream = {
   },
 };
 
-const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Invalid Endpoint');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`Server started on port ${port}`);
+});
+const io = require('socket.io').listen(server);
+
+io.on('connection', (socket) => {
+  logger.info('User connected');
+
+  socket.on('disconnect', () => {
+    logger.info('User disconnected');
+  });
+
+  socket.on('player:join', (msg) => {
+    logger.info('Player Join', msg);
+  });
 });
